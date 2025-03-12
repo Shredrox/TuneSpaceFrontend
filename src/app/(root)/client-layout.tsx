21044c1e76b2c701f2e09620c371285abee57742
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-03-12 22:35:47 +0200

    remove unneeded elements

--- src/app/(root)/client-layout.tsx
+++ src/app/(root)/client-layout.tsx
@@ -31,17 +31,7 @@ export default function MainClientLayout({
         <div className="w-full h-screen flex justify-center flex-col items-center gap-12">
           <h1 className="text-7xl">TuneSpace</h1>
           <div className="flex justify-center items-center gap-4">
-            {/* <div className="flex flex-col  justify-center items-center">
-            <span className="text-xl">Are you an artist?</span>
-            <span>Register here</span>
-            <Button>
-              <Link href="band/register">Register</Link>
-            </Button>
-          </div> */}
             <div className="flex flex-col justify-center items-center w-full">
-              {/* <span className="text-xl">Want to discover bands?</span>
-            <span>Enter here</span> */}
-              {/* <AccessForm shouldRedirect={true} /> */}
               <AuthForm className="w-[750px]" />
             </div>
           </div>
