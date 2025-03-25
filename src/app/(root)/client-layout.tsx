39dc13250025a7c4286d150743b7b0fcb7bfc678
Shredrox <93082689+Shredrox@users.noreply.github.com>
2025-03-25 22:39:48 +0200

    add refresh token

--- src/app/(root)/client-layout.tsx
+++ src/app/(root)/client-layout.tsx
@@ -3,8 +3,11 @@
 import { AppSidebar } from "@/components/AppSidebar";
 import Header from "@/components/Header";
 import { AuthForm } from "@/components/auth/AuthForm";
+import Loading from "@/components/fallback/Loading";
 import { SidebarProvider } from "@/components/shadcn/Sidebar";
 import useAuth from "@/hooks/useAuth";
+import useRefreshToken from "@/hooks/useRefreshToken";
+import { useEffect, useState } from "react";
 
 export default function MainClientLayout({
   children,
@@ -12,6 +15,27 @@ export default function MainClientLayout({
   children: React.ReactNode;
 }) {
   const { auth } = useAuth();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   if (auth.accessToken) {
     return (
